78988b29c42c4a96b15ade35c3938ceb71c5244a
Omar Aparicio <omaraparicio07@gmail.com>
2019-08-12 01:26:09 -0500

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="appRoot">
+    <p>{{mensaje}}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'indexPage',
+  data () {
+    return {
+      mensaje: 'Hola desde un Vue'
+    }
+  },
+  created () { // lifecycle hook
+    this.getMessage()
+  },
+  methods: {
+    getMessage () {
+      axios.get('http://localhost:5000/api/v1.0/message').then((response) => {
+        this.mensaje = response.data
+      }).catch((error) => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
