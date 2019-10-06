3f9aad48ed9b857b21936afbdec930774a34a90e
Omar Aparicio <omaraparicio07@gmail.com>
2019-10-06 12:23:04 -0500

    SCT-1 Añadiendo navbar en App.vue, configurando rutas para la navegacion basica

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
+    <!-- NavBar -->
+     <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand to="/" class="text-white font-weight-bold"> Schemas Converter Tool </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item to="/ERSchemaEditor" class="font-weight-bold text-white" exact>ER Schema Editor </b-nav-item>
+            <b-nav-item to="/RelationalSchema" class="font-weight-bold text-white"> Relational Schema </b-nav-item>
+            <b-nav-item to="/NoRelationalSchema" class="font-weight-bold text-white"> No Relational Schema </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
     <router-view/>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home  from "../views/Home"
+import ERSchemaEditor from '../views/ERSchemaEditor'
+import RelationalSchema from '../views/RelationalSchema'
+import NoRelationalSchema from '../views/NoRelationalSchema'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Home',
-      component: Home
+      path:'/',
+      name:'Home',
+      component:Home
+    },
+    {
+      path: '/ERSchemaEditor',
+      name: 'ERSchemaEditor',
+      component: ERSchemaEditor
+    },
+    {
+      path:'/RelationalSchema',
+      name:'RelationalSchema',
+      component:RelationalSchema
+    },
+    {
+      path: '/NoRelationalSchema',
+      name: 'NoRelationalSchema',
+      component: NoRelationalSchema
     }
   ],
   mode: 'history'
